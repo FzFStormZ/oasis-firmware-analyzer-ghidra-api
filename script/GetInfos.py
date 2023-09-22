bed9096017d307c576441eb7bbb34ef8bda3b2e8
FzFStormZ <kali@kali.workgroup>
2023-09-22 11:37:35 +0200

    first commit (script + tables added)

---
+++ script/GetInfos.py
@@ -0,0 +1,40 @@
+#TODO write a description for this script
+#@author 
+#@category Search.InstructionPattern
+#@keybinding 
+#@menupath 
+#@toolbar 
+
+from ghidra.program.model.lang import OperandType
+
+addr = "000527c4"
+
+instruc = currentProgram.getListing().getInstructionAt(toAddr(addr))
+
+#1
+operandTypeList = []
+#4
+nboperands = instruc.getNumOperands()
+#5
+operandList = []
+#6
+mnemonic = instruc.getMnemonicString()
+
+pcode_op = ""
+for pcode in instruc.getPcode():
+	p = pcode.getOpcode()
+	pcode_op = pcode_op + "," + str(p)
+print(pcode_op)
+
+for k in range(nboperands):
+	#1
+	operandTypeList.append(instruc.getOperandType(k))
+	#5
+	operandList.append(instruc.getOpObjects(k))
+
+if nboperands == 3:
+	print(mnemonic + "," + str(nboperands) + ",(" + str(operandTypeList[0]) + ":" + str(operandList[0][0]) + "),(" + str(operandTypeList[1]) + ":" + str(operandList[1][0]) + "),(" + str(operandTypeList[2]) + ":" + str(operandList[2][0]) + ")")
+elif nboperands == 2:
+	print(mnemonic + "," + str(nboperands) + ",(" + str(operandTypeList[0]) + ":" + str(operandList[0][0]) + "),(" + str(operandTypeList[1]) + ":" + str(operandList[1][0]) + ")")
+elif nboperands == 1:
+	print(mnemonic + "," + str(nboperands) + ",(" + str(operandTypeList[0]) + ":" + str(operandList[0][0]) + ")")
